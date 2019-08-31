b7e9e6dd10886c097dc510eb31490bf9a4120d4a
Your Name <1298881987@qq.comjotting-webapp hcb$ git config --global user.name hcbgit config --global user.email 1298881987@qq.com>
2019-08-31 22:39:27 +0800

    redis demo

---
+++ src/main/java/com/hcb/redisDemo/RedisDemoTest.java
@@ -0,0 +1,47 @@
+package com.hcb.redisDemo;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+public class RedisDemoTest {
+
+    @Test
+    public void redisTester() {
+        Jedis jedis = new Jedis("localhost", 6379, 100000);
+        int i = 0;
+        try {
+            long start = System.currentTimeMillis();// 开始毫秒数
+            while (true) {
+                long end = System.currentTimeMillis();
+                if (end - start >= 1000) {// 当大于等于1000毫秒（相当于1秒）时，结束操作
+                    break;
+                }
+                i++;
+                jedis.set("test" + i, i + "");
+
+            }
+        } finally {// 关闭连接
+            jedis.close();
+        }
+        // 打印1秒内对Redis的操作次数
+        System.out.println("redis每秒操作：" + (i+1) + "次");
+    }
+
+    @Test
+    public void redisTester2() {
+        Jedis jedis = new Jedis("localhost", 6379, 100000);
+        int sum=7932;
+        try {
+            for (int i = 0; i < sum; i++) {
+                jedis.del("test" + i);
+            }
+
+        } finally
+        {// 关闭连接
+            jedis.close();
+        }
+        // 打印1秒内对Redis的操作次数
+//            System.out.println("redis每秒操作："+i +"次");
+
+    }
+}
